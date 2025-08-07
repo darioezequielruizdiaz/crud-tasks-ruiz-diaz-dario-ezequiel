c58e7efcaf834f1a4980af2a490c6cb735ca290b
Dario Ezequiel Ruiz Diaz <darioezequiel.ruizdiaz@gmail.com>
2025-08-07 10:43:42 -0300

    🔧 feat: se añadio la configuracion del servidor como de la conexion a la base de datos

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import chalk from "chalk";
+import dotenv from "dotenv";
+import { startDB } from "./src/config/database.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+// Hacemos que la app escuche el puerto e iniciamos la base de datos
+app.listen(PORT, async () => {
+
+    await startDB();
+    console.log(`EL servidor se está ejecutando en ${chalk.bgMagenta(`http://localhost:${PORT}/`)}`);
+});
