d0e8ffe39f4c27c7a01a14dbdccd613c668a382f
Dario Ezequiel Ruiz Diaz <darioezequiel.ruizdiaz@gmail.com>
2025-08-07 10:52:34 -0300

    🌟 feat: Se añadio el modelo de Task

---
+++ src/models/task.model.js
@@ -0,0 +1,22 @@
+import { sequelize } from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+const Task = sequelize.define(
+    "tasks", {
+        title: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true
+        },
+        description: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        isComplete: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    }
+)
+
+export default Task;
