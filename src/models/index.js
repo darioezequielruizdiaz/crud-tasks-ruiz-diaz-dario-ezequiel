ec32179d8395c5c0ad92e270cccaeb277a2fa1b6
Dario Ezequiel Ruiz Diaz <darioezequiel.ruizdiaz@gmail.com>
2025-08-18 10:31:56 -0300

    ✅ se configuro las relaciones en el index de tag y tasktag

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 import User from "./user.model.js";
 import Task from "./task.model.js";
 import Profile from "./profile.model.js";
+import { Tag, TaskTag } from "./tag.model.js"
 
 // Relacion uno a muchos: User - Task
 User.hasMany(Task, { foreignKey: "user_id" });
@@ -10,8 +11,15 @@ Task.belongsTo(User, { foreignKey: "user_id" });
 User.hasOne(Profile, { foreignKey: "user_id" });
 Profile.belongsTo(User, { foreignKey: "user_id" });
 
+// Relacion muchos a muchos: Task - Tag
+Task.belongsToMany(Tag, { through: TaskTag, foreignKey: "task_id" });
+Tag.belongsToMany(Task, { through: TaskTag, foreignKey: "tag_id" });
+
+
 export {
   User,
   Task,
-  Profile
+  Profile,
+  Tag,
+  TaskTag
 };
