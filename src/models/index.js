c9688e034a2902109f8e6771be9e0d613c22f20c
Dario Ezequiel Ruiz Diaz <darioezequiel.ruizdiaz@gmail.com>
2025-08-18 10:00:31 -0300

    ✅ se configuro las relaciones en el index

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,14 @@
 import User from "./user.model.js";
 import Task from "./task.model.js";
+import Profile from "./profile.model.js";
 
 // Relacion uno a muchos: User - Task
 User.hasMany(Task, { foreignKey: "user_id" });
 Task.belongsTo(User, { foreignKey: "user_id" });
+
+// Relacion uno a uno: User - Profile
+User.hasOne(Profile, { foreignKey: "user_id" });
+Profile.belongsTo(User, { foreignKey: "user_id" });
 
 export {
   User,
