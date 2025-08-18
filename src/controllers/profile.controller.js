8809245743178a935f17b59dc0de6c760d104190
Dario Ezequiel Ruiz Diaz <darioezequiel.ruizdiaz@gmail.com>
2025-08-18 10:07:43 -0300

    ✅ se añadio un controllador de profile

---
+++ src/controllers/profile.controller.js
@@ -0,0 +1,38 @@
+import { Profile, User } from "../models/index.js";
+
+export const createProfile = async (req, res) => {
+
+    const { bio, user_id } = req.body;
+
+    if (!user_id) return res.status(400).json({ error: "🚫 user_id es requerido 🚫" });
+
+    try {
+        
+        const user = await User.findByPk(user_id);
+        if (!user) return res.status(404).json({ error: "🚫 Usuario no encontrado 🚫" });
+        
+        const profile = await Profile.create({ bio, user_id });
+        return res.status(201).json(profile);
+        
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+};
+
+export const getAllProfiles = async (req, res) => {
+
+    try {
+
+        const profiles = await Profile.findAll({
+          include: { 
+            model: User, 
+            attributes: ["id", "name", "email"] 
+        }});
+        return res.json(profiles);
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+};
